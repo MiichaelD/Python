c051d421483dfe4471bd765d89c410ef79d08c40
Michael Duarte <michael.duarte@hotmail.com>
2016-04-14 22:13:52 -0700

    Retry request if response is empty or an exception occurs

--- RegistroDelConsumidor.py
+++ RegistroDelConsumidor.py
@@ -10,29 +10,47 @@ More Info:	http://repep.profeco.gob.mx/index.jsp
 		https://www.facebook.com/Frontera.info/videos/10153530275841961/ 
 		https://www.youtube.com/watch?v=7TW4lNE45hk
 
-Date:		April 13th, 2016
+Date:			April 13th, 2016
 '''
 
 import httplib, urllib
+import time
 headers = {'Host':'repep.profeco.gob.mx',
 			'Content-Type':'application/x-www-form-urlencoded',
 			'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 			'Referer':'http://repep.profeco.gob.mx/registrar_telefono.jsp'
 		 	}
-payload_format = "Usuario=CONSUMIDOR&lada={}&phone={}&phonee={}&ext=&checkbox1=Comercio&checkbox2=Telecomunicaciones\
+
+payload_format = "Usuario=CONSUMIDOR&lada={}&phone={}&phonee={}{}&ext=&checkbox1=Comercio&checkbox2=Telecomunicaciones\
 			&checkbox3=Tur%EDstico&Nsector=3&Pagina=1&telefono=&Submit=Aceptar"
-host = "repep.profeco.gob.mx:80" # NO 'protocol://'
+host = "repep.profeco.gob.mx" # NO 'protocol://'
+port = '80'
+strict = False
+timeout = 15 #secs
+sleep_time = 5
 receiver_script = '/RegistroConsumidor'
 
 lada = '686'
 for i in range(1000000,9999999):
 	phone = format(i, '07')
-	payload = payload_format.format(lada, phone, lada+phone)
+	payload = payload_format.format(lada, phone, lada, phone)
+	while True:
+		try:
+			conn = httplib.HTTPConnection(host, port, strict, timeout)
+			conn.request("POST", receiver_script, payload, headers)
+			response = conn.getresponse()
+			data = response.read()
+			print lada+phone, ': ', response.status, response.reason, data
 
-	conn = httplib.HTTPConnection(host)
-	conn.request("POST", receiver_script, payload, headers)
-	response = conn.getresponse()
-	print response.status, response.reason, response.read()
+			if len(data) > 0:
+				break;
+
+			print '\tEmpty data, trying again...'
+			time.sleep(sleep_time)
+				
+		except Exception as e:
+			print '\tThere was an exception: {}, trying again...'.format(e);
+			time.sleep(sleep_time)
 
 
 '''
